e7b1c29dbc77dd347bc91362e97ee0c53d6acd08
emily <emily@bacheconsulting.com>
2025-12-16 13:58:16 +0100

    separate functional core from imperative shell

--- memories/2023/exercise/java/day07/src/main/java/ci/Pipeline.java
+++ memories/2023/exercise/java/day07/src/main/java/ci/Pipeline.java
@@ -18,18 +18,28 @@ public class Pipeline {
         this.log = log;
     }
 
+    /**
+     * This is supposed to be the functional core
+     */
     public void run(Project project) {
+        var steps = createBuildPipeline();
         var pipelineStatus = new PipelineStatus();
+        for (var step: steps) {
+            step.run(project, pipelineStatus);
+        }
+    }
+
+    /**
+     * This is supposed to be the imperative shell
+     */
+    private List<PipelineStep> createBuildPipeline() {
         var testStep = new TestStep("tests", log);
         var deployStep = new DeployStep("deploy", log);
         var reportStep = new ReportStep("report", log, emailer, config);
 
-        var steps = List.of(
+        return List.of(
                 testStep, deployStep, reportStep
         );
-        for (var step: steps) {
-            step.run(project, pipelineStatus);
-        }
     }
 
 }
