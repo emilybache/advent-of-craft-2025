e73ceeefb401e59160d76b1350543374738fce12
emily <emily@bacheconsulting.com>
2025-12-16 11:52:15 +0100

    pipeline steps

--- memories/2023/exercise/java/day07/src/main/java/ci/Pipeline.java
+++ memories/2023/exercise/java/day07/src/main/java/ci/Pipeline.java
@@ -17,12 +17,13 @@ public class Pipeline {
     }
 
     public void run(Project project) {
-        var result = doTestStep(project);
-        var deployResult = doDeployStep(project, result);
-        doReportStep(result, deployResult);
+
+        var result = doTestStep(project, log);
+        var deployResult = doDeployStep(project, result, log);
+        doReportStep(result, deployResult, log, emailer, config);
     }
 
-    private TestStepResult doTestStep(Project project) {
+    private TestStepResult doTestStep(Project project, Logger log) {
         boolean testsPassed;
         if (project.hasTests()) {
             if ("success".equals(project.runTests())) {
@@ -39,7 +40,7 @@ public class Pipeline {
         return new TestStepResult(testsPassed);
     }
 
-    private DeployStepResult doDeployStep(Project project, TestStepResult result) {
+    private DeployStepResult doDeployStep(Project project, TestStepResult result, Logger log) {
         boolean deploySuccessful;
         if (result.testsPassed()) {
             if ("success".equals(project.deploy())) {
@@ -55,7 +56,7 @@ public class Pipeline {
         return new DeployStepResult(deploySuccessful);
     }
 
-    private void doReportStep(TestStepResult result, DeployStepResult deployResult) {
+    private void doReportStep(TestStepResult result, DeployStepResult deployResult, Logger log, Emailer emailer, Config config) {
         if (config.sendEmailSummary()) {
             log.info("Sending email");
             if (result.testsPassed()) {
